4a08c56151a4587d45d8a3e2cb54685e217886d3
Yusuf Lawal <yusuf.olaoluwalawal@outlook.com>
2022-08-26 00:14:20 +0100

    new commit

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const navs = document.querySelectorAll('.inner-menu .pages li');
 btn.addEventListener('click', () => {
     menu.classList.toggle('open');
     btn.classList.toggle('open');
+    document.body.classList.toggle('no-scroll')
 });
 
 navs.forEach((nav) => {
     nav.addEventListener('click', () => {
         menu.classList.remove('open');
         btn.classList.remove('open');
+        document.body.classList.toggle('no-scroll')
     })
 })
